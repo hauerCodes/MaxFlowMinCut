e7995b0e76ea706c0c1c89925a54b11b02c958e5
Markus Zytek <markus.zytek@gmail.com>
2015-01-23 12:15:12 +0000

    First Check In

---
+++ MaxFlowMinCut/MaxFlowMinCut.Lib/Network/Edge.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Edge.cs" company="FH Wr. Neustadt">
+//   Christoph Hauer & Markus Zytek
+// </copyright>
+// <summary>
+//   The edge.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace MaxFlowMinCut.Lib.Network
+{
+    using System;
+
+    /// <summary>
+    /// The edge.
+    /// </summary>
+    [Serializable]
+    public class Edge
+    {
+        public Node From { get; set; }
+
+        public Node To { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Flow { get; set; }
+
+        public int Label { get; set; }
+    }
+}
---
+++ MaxFlowMinCut/MaxFlowMinCut.Wpf/Model/InputEdge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxFlowMinCut.Wpf.Model
+{
+    public class InputEdge
+    {
+        public InputEdge(string nodeFrom, string nodeTo, int capacity)
+        {
+            this.NodeFrom = nodeFrom;
+            this.NodeTo = nodeTo;
+            this.Capacity = capacity;
+        }
+
+        public string NodeFrom { get; set; }
+
+        public string NodeTo { get; set; }
+
+        public int Capacity { get; set; }
+    }
+}
